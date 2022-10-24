90bf625dbaf1c8f908209c3616c2347a0b5ee178
jaimeeramiirez <113980639+jaimeeramiirez@users.noreply.github.com>
2022-10-24 20:59:10 +0000

    informacion cartas y reglas del juego creadas

--- main.py
+++ main.py
@@ -1,30 +1,5 @@
 #EJERCICIO BLACK JACK
 
-print("\n\n")
+import reglas_juego
 
-cartas = {
-    chr(0x1f0a1): 11,
-    chr(0x1f0a2): 2,
-    chr(0x1f0a3): 3,
-    chr(0x1f0a4): 4,
-    chr(0x1f0a5): 5,
-    chr(0x1f0a6): 6,
-    chr(0x1f0a7): 7,
-    chr(0x1f0a8): 8,
-    chr(0x1f0a9): 9,
-    chr(0x1f0aa): 10,
-    chr(0x1f0ab): 10,
-    chr(0x1f0ad): 10,
-    chr(0x1f0ae): 10,
-}
-
-print(cartas)
-print("\n\n")
-print("Cartas: {}".format(" ".join(cartas.keys())))
-print("\n")
-print("Putos: {}".format(list(cartas.values())))
-
-print("\n")
-
-for carta, valor in cartas.items():
-    print("La carta {} vale {} puntos".format(carta, valor))
+import informacion_cartas
---
+++ reglas_juego.py
@@ -0,0 +1,6 @@
+def reglas():
+  print("*"*9, "Objetivo", "*"*9, "En el BlackJack, también llamado Veintiuno, el objetivo fundamental es ganar al croupier con tus cartas que deben sumar un valor de 21 o lo más cercano a 21 sin pasarnos (A sacar exactamente 21 se le llama Blackjack) \n\n Tal y como explicábamos al principio, un jugador gana si se dan los siguientes   supuestos:\n -No ha superado los 21 puntos y su puntuación, sea cual sea, supera la del croupier.\n -Llega a los 21 puntos con un BlackJack\nDe igual forma, pierde si:\n -Es superado en puntuación por el croupier, y éste no se pasa de 21 puntos Si se pasa de 21 puntos")
+print("\n\n")
+
+reglas()
+print("\n\n")
